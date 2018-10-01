bad30b8c3fbf8cf06b8d1cf7a5edc4019bd2888b
bin16 <bin16@outlook.com>
2018-10-02 01:15:17 +0800

    homepage

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const router = express.Router()
 
+const { groupBy } = require('lodash')
+
+const { News } = require('../db')
+
 router
-    .get('/', function(_, res) {
-        res.render('index', { title: 'Express' })
+    .get('/', async (_, res) => {
+        const _base = await News.find({
+            status: 'active',
+            level: { $in: ['normal', 'featured'] },
+            category: { $in: ['news', 'event', 'notice', 'alert', 'project'] },
+        }, null, { sort: '-createdAt' })
+        const mustread = await News.find({
+            status: 'active',
+            level: 'mustread',
+        }, null, {
+            sort: '-createdAt',
+        })
+
+        await res.render('index', {
+            ...groupBy(_base, 'type'),
+            mustread
+        })
     })
     .get('/login', (req, res) => res.render('login'))
     .get('/cb/success', (req, res) => res.render('success'))
+
+
+
 
 module.exports = router
